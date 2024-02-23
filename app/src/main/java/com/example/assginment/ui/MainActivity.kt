b7398249e695541cdedcdf1eaf3ccab87788375f
Shivang_Yadav <shivangyadav469@gmail.com>
2024-02-24 02:04:15 +0530

    Implemented BottomNav, navHost frag, styles, colors as required

--- app/src/main/java/com/example/assginment/ui/MainActivity.kt
+++ app/src/main/java/com/example/assginment/ui/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.assginment.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.assginment.R
+import com.example.assginment.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var bottomNav: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv) as NavHostFragment
+        navController = navHostFragment.navController
+        bottomNav = binding.bnv
+        bottomNav.setupWithNavController(navController)
     }
 }
